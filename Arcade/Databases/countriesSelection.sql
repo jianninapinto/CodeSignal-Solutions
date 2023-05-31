5f7b666dc1201920c69bfe6000e0b09665338ae5
Jiannina Pinto <52984159+jianninapinto@users.noreply.github.com>
2023-05-31 11:24:35 -0500

    Delete Arcade directory

--- Arcade/Databases/countriesSelection.sql
+++
@@ -1,8 +0,0 @@
-CREATE PROCEDURE solution()
-BEGIN
-	/* Write your SQL here. Terminate each statement with a semicolon. */
-	SELECT *
-	FROM countries
-	WHERE continent = 'Africa'
-	ORDER BY name;	
-END
