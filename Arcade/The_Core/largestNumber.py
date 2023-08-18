3781bcb0082200454e4b46e9c8a2be1e6ff17adc
Jiannina <Yanin@Jianninas-MacBook-Pro.local>
2023-08-18 14:46:30 -0500

    circle sol and rearrangement

---
+++ Arcade/The_Core/largestNumber.py
@@ -0,0 +1,28 @@
+def solution(n):
+    s = str(9)
+    m = int(s * n)
+    return m
+
+"""
+Given an integer n, return the largest number that contains exactly n digits.
+
+Example
+
+For n = 2, the output should be
+solution(n) = 99.
+
+Input/Output
+
+[execution time limit] 4 seconds (py3)
+
+[memory limit] 1 GB
+
+[input] integer n
+
+Guaranteed constraints:
+1 ≤ n ≤ 9.
+
+[output] integer
+
+The largest integer of length n.
+"""
