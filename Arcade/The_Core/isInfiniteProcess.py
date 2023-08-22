76fc046614f301500d0623c74e3a9d27f5775ccc
Jiannina <Yanin@Jianninas-MacBook-Pro.local>
2023-08-22 15:49:42 -0500

    sol infinite process

---
+++ Arcade/The_Core/isInfiniteProcess.py
@@ -0,0 +1,43 @@
+def solution(a, b):
+    if a > b:
+        return True 
+
+    if (b - a) % 2 == 0:
+        return False
+    else:
+        return True
+    
+    """
+    Given integers a and b, determine whether the following pseudocode results in an infinite loop
+
+while a is not equal to b do
+  increase a by 1
+  decrease b by 1
+Assume that the program is executed on a virtual machine which can store arbitrary long numbers and execute forever.
+
+Example
+
+For a = 2 and b = 6, the output should be
+solution(a, b) = false;
+For a = 2 and b = 3, the output should be
+solution(a, b) = true.
+Input/Output
+
+[execution time limit] 4 seconds (py3)
+
+[memory limit] 1 GB
+
+[input] integer a
+
+Guaranteed constraints:
+0 ≤ a ≤ 20.
+
+[input] integer b
+
+Guaranteed constraints:
+0 ≤ b ≤ 20.
+
+[output] boolean
+
+true if the pseudocode will never stop, false otherwise.
+    """
