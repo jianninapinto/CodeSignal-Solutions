08fa89baac0ec2e69d63019a022732e823a95e16
Jiannina <Yanin@Jianninas-MacBook-Pro.local>
2023-09-22 23:25:04 -0500

    array sol

---
+++ Arcade/The_Core/createArray.py
@@ -0,0 +1,27 @@
+def solution(size):
+    array = [1] * size
+    return array
+
+"""
+Given an integer size, return array of length size filled with 1s.
+
+Example
+
+For size = 4, the output should be
+solution(size) = [1, 1, 1, 1].
+
+Input/Output
+
+[execution time limit] 4 seconds (py3)
+
+[memory limit] 1 GB
+
+[input] integer size
+
+A positive integer.
+
+Guaranteed constraints:
+1 ≤ size ≤ 1000.
+
+[output] array.integer
+"""
